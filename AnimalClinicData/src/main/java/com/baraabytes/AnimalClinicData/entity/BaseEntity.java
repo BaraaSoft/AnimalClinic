cd6e627e7064812000bb47dc3458e7c07e48498a
baraa <baraaabuzeid@hotmail.com>
2020-12-30 21:50:18 +0200

    - add entity Id. closes #1.

---
+++ AnimalClinicData/src/main/java/com/baraabytes/AnimalClinicData/entity/BaseEntity.java
@@ -0,0 +1,13 @@
+package com.baraabytes.AnimalClinicData.entity;
+
+public class BaseEntity {
+    private long id;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
--- AnimalClinicData/src/main/java/com/baraabytes/AnimalClinicData/entity/Person.java
+++ AnimalClinicData/src/main/java/com/baraabytes/AnimalClinicData/entity/Person.java
@@ -1,6 +1,6 @@
 package com.baraabytes.AnimalClinicData.entity;
 
-public class Person {
+public class Person extends BaseEntity {
     private String firstname;
     private String lastname;
 
